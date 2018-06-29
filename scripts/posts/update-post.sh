df8a2978b752c833cf931e37547dc6f8e2cbbdf4
KatherineWard <katherine.eileen.ward@gmail.com>
2018-06-29 09:17:15 -0400

    updated post serializer to include relationship to

--- scripts/posts/update-post.sh
+++ scripts/posts/update-post.sh
@@ -4,7 +4,7 @@ curl "http://localhost:4741/posts/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=${token}" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
       "post": {
         "post_id": "'"${POSTID}"'",
